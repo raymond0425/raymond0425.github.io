ec63b52e8b93605664627b165e705ac12b63d0f7
raymond0425 <53153236+raymond0425@users.noreply.github.com>
2025-06-03 06:04:57 +0800

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
     <section id="resume">
       <h1>Resume</h1>
       <div className="pdf-container">
-        <iframe src="/CV.pdf" className="pdf-embed"></iframe>
+        <iframe src="/CV2.pdf" className="pdf-embed"></iframe>
       </div>
     </section>
   );
